66ae1c3bb6e3ee33621f010932a179c2e9838e5b
Cristian Zapata <c.zapatat@uniandes.edu.co>
2024-10-22 00:19:07 -0500

    Update mail_bp.py

--- blueprints/mail_bp.py
+++ blueprints/mail_bp.py
@@ -37,7 +37,7 @@ def validate_authorization(authorization):
 
 @mail_blueprint.route("/", methods=["GET"])
 def healthcheck():
-    return jsonify({'status': 'UP'})
+    return jsonify({'status': 'UP', 'version': 2})
 
 # Recurso que expone la funcionalidad reset blacklist
 @mail_blueprint.route('/blacklists/reset', methods=['POST'])
