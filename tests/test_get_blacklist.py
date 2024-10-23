42f47b5264b96e93bb7d635caac8df15709151c1
cristian.zapata <cristian.zapata@rappi.com>
2024-10-22 22:43:48 -0500

    Test de Obtener Email

--- tests/test_get_blacklist.py
+++ tests/test_get_blacklist.py
@@ -1,7 +1,6 @@
 from unittest.mock import patch, MagicMock
 from flask import Flask
 from src.blueprints.mail_bp import mail_blueprint
-from src.errors.errors import InvalidDataError, ApiError
 
 
 class TestGetBlacklist():
@@ -13,18 +12,33 @@ class TestGetBlacklist():
         app_context.push()
         self.application.register_blueprint(mail_blueprint)
 
-    @patch('src.models.mail.MailBlocked.query')
-    def test_query_blacklist(self, mock_query):
+    @patch('src.blueprints.mail_bp.db')
+    def test_query_blacklist_false(self, mock_db):
         # Set up
-        mock_query.filter.side_effect = {
-            "email":"jazzjhon1@gmail.com",
-            "app_uuid":"85482912-afc3-4147-a458-9ac4b99242bd",
-            "blocked_reason":"spam"
-        }
-        token = 'api_secret'
-        headers = {'Authorization': 'Bearer valid_token'}
-        if token:
-            headers.update({'Authorization': f'Bearer {token}'})
+        mock_db.session.query().filter_by().one_or_none.return_value = None
+        headers = {'Authorization': 'Bearer api_secret'}
+
         with self.application.test_client() as test_client:
             self.response = test_client.get('/blacklists/jazzjhon@gmail.com', headers=headers)
-            self.response_json = self.response.json
+            assert self.response.status_code == 200
+            assert self.response.json == {
+                'is_blocked': False,
+                'blocked_reason': ''
+            }
+
+    @patch('src.blueprints.mail_bp.db')
+    def test_query_blacklist_true(self, mock_db):
+        # Set up
+        mock_mail_blocked = MagicMock()
+        mock_mail_blocked.email = 'test@example.com'
+        mock_mail_blocked.blocked_reason = 'spam'
+        mock_db.session.query().filter_by().one_or_none.return_value = mock_mail_blocked
+        headers = {'Authorization': 'Bearer api_secret'}
+
+        with self.application.test_client() as test_client:
+            self.response = test_client.get('/blacklists/test@example.com', headers=headers)
+            assert self.response.status_code == 200
+            assert self.response.json == {
+                'is_blocked': True,
+                'blocked_reason': 'spam'
+            }
